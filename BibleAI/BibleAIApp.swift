0f0d021d771aa752fa1ac69ceb61ce51b3dfaff9
Maria Green <theearthdiva@gmail.com>
2023-01-14 13:55:23 -0500

    Initial Commit

---
+++ BibleAI/BibleAIApp.swift
@@ -0,0 +1,17 @@
+//
+//  BibleAIApp.swift
+//  BibleAI
+//
+//  Created by Maria Green on 1/14/23.
+//
+
+import SwiftUI
+
+@main
+struct BibleAIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
